757bdf21d4fd61a39911b358442740fb43c4d6c9
troy dorman <troydorman@gmail.com>
2024-10-26 07:44:28 -0600

    set up routes for each component in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ItemsComponent } from './components/items/items.component';
+import { WarehousesComponent } from './components/warehouses/warehouses.component';
 
-export const routes: Routes = [];
+import { EmployeesComponent } from './components/employees/employees.component';
+import { InventorylogComponent } from './components/inventorylog/inventorylog.component';
+
+import { SuppliersComponent } from './components/suppliers/suppliers.component';
+
+export const routes: Routes = [
+  { path: 'items', component: ItemsComponent },
+  { path: 'warehouses', component: WarehousesComponent },
+  { path: 'suppliers', component: SuppliersComponent },
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'inventory-log', component: InventorylogComponent },
+  { path: '', redirectTo: '/items', pathMatch: 'full' },
+];
