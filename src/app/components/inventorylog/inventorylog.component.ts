7966ea68d4932b2511115201bff0d19863eceb2e
troy dorman <troydorman@gmail.com>
2024-10-26 07:32:59 -0600

    created components and services

---
+++ src/app/components/inventorylog/inventorylog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventorylog',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventorylog.component.html',
+  styleUrl: './inventorylog.component.scss'
+})
+export class InventorylogComponent {
+
+}
